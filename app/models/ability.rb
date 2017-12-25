84db415eab52e5dcca8d88a98222c76f1467ec2a
Almir  Santos Filho <almir@MacBook-Pro-de-Almir.local>
2017-12-25 18:31:39 -0200

    fixed the :read clause

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     if user
-      can :read, all
+      can :read, :all
       can :manage, Post, user_id: user.id
       can :manage, Follow, user_id: user.id
       can :manage, Like, user_id: user.id
