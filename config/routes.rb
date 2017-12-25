87b34856498654e341c5ad1c04e8e681a6c0259b
Almir  Santos Filho <almir@MacBook-Pro-de-Almir.local>
2017-12-25 15:13:11 -0200

    Define all routes for the current resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers => {registrations: 'registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "pages#home"
+
+  resources :likes, only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy, :edit, :update]
+  resources :posts, only: [:index, :show, :create, :destroy, :edit, :update]
+  resources :follows, only: [:create, :destroy]
+
+  get "/profile/:id", to: "users#show", as: "user"
+  get '/profile/:id/followings', to: "users#followings", as: "followings"
+  get '/profile/:id/followers', to: "users#followrs", as: "followers"
+  post "/users/search", to: "users#search", as: "search"
+
+
+  devise_for :users, :controllers => {registrations: 'registrations'}
 end
