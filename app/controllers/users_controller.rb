b354e7cbe57c7ddbda642688fd1aac9b7b45326f
Almir  Santos Filho <almir@MacBook-Pro-de-Almir.local>
2017-12-25 18:30:36 -0200

    show, followers, following, search done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,30 @@
 class UsersController < ApplicationController
   load_and_authorize_resource
+  before_action :set_user, only: [:show, :following, :followers]
+
   def show
+    @post = @user.posts
+    @following = @user.following_users.limit(5)
+    @followers = @user.followers_by_type("User").limit(5)
   end
 
   def followers
+    @followers = @user.following_users
   end
 
-  def followings
+  def following
+    @following = @user.following_users
   end
 
   def search
+    @user = current_user
+    @query = User.ransak(params[:q])
+    @user = @query.result(distinct: true)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
